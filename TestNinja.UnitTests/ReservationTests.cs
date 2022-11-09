8018ecc9332814c0dd48738df0239279f2138c49
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-09 22:12:16 +0400

    Add project files.

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelled(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingReservation_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelled(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNotAdmin_ReturnsFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelled(new User { IsAdmin = false });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+       
+    }
+}
---
+++ TestNinja/Fundamentals/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Fundamentals
+{
+    public class Reservation
+    {
+        public User? MadeBy { get; set; }
+        
+        public bool CanBeCancelled(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+            if (MadeBy == user)
+                return true;
+
+            return false;
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
