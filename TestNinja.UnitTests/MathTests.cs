881f65264a3ef4cb8bd4a0658c0d64d2c1161b8a
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-11 16:20:37 +0400

    feat: Added new tests for Max Method in Math class

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -3,16 +3,47 @@ using TestNinja.Fundamentals;
 using Math = TestNinja.Fundamentals.Math;
 
 namespace TestNinja.UnitTests;
+
 [TestFixture]
 public class MathTests
 {
-     [Test]
-     public void Add_WhenCalled_ReturnSumOfArguments()
-     {
-          var math = new Math();
+    [Test]
+    public void Add_WhenCalled_ReturnSumOfArguments()
+    {
+        var math = new Math();
 
-          var result = math.Add(1, 2);
-          
-          Assert.That(result, Is.EqualTo(3));
-     }
+        var result = math.Add(1, 2);
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Max_FirstArgumentGreater_ReturnTheFirstArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(2, 1);
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Max_SecondArgumentGreater_ReturnTheSecondArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(1, 2);
+        
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(1, 1);
+        
+        Assert.That(result, Is.EqualTo(1));
+    }
 }
