9f34e1072d4f568f1e183ecc65a00bf3350cf1d0
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-17 17:34:14 +0400

    feat: Testing DemeritPointsCalculator

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class DemeritPointsCalculatorTests
+{
+    [Test]
+    public void CalculateDemeritPoints_SpeedIsNotGreaterZero_ThrowsArgumentOutOfRangeException()
+    {
+        var demerit = new DemeritPointsCalculator();
+
+        // var results = demerit.CalculateDemeritPoints(-1);
+        
+        Assert.That(()=> demerit.CalculateDemeritPoints(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException> ());
+    }
+
+    [Test]
+    public void CalculateDemeritPoints_SpeedIsNotGreaterSpeedLimit_ReturnZero()
+    {
+        var demerit = new DemeritPointsCalculator();
+
+        var results = demerit.CalculateDemeritPoints(30);
+        
+        Assert.That(results, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void CalculateDemeritPoints_SpeedGreaterSpeedLimit_ReturnDemeritPoint()
+    {
+        var demerit = new DemeritPointsCalculator();
+
+        var results = demerit.CalculateDemeritPoints(70);
+        
+        Assert.That(results, Is.EqualTo(1));
+    }
+}
