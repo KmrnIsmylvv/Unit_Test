303e7f500ebd99c75881b59bff948fe327bdfd56
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-17 16:51:14 +0400

    feat: Testing Strings again

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    public void GetOutput_DivideThreeAndFive_ReturnFizzBuzz()
+    {
+        var result = FizzBuzz.GetOutput(15);
+        
+        Assert.That(result,Is.EqualTo("FizzBuzz"));
+    }
+
+    [Test]
+    public void GetOutput_DivideThreeOnly_ReturnFizz()
+    {
+        var result = FizzBuzz.GetOutput(3);
+        
+        Assert.That(result, Is.EqualTo("Fizz"));
+    }
+
+    [Test]
+    public void GetOutput_DivideFiveOnly_ReturnBuzz()
+    {
+        var result = FizzBuzz.GetOutput(5);
+        
+        Assert.That(result, Is.EqualTo("Buzz"));
+    }
+
+    [Test]
+    public void GetOutput_NotDivideThreeAndFive_ReturnNumber()
+    {
+        var result = FizzBuzz.GetOutput(7);
+        
+        Assert.That(result, Is.EqualTo("7")); 
+    }
+
+}
