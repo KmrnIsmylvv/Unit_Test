346bffee0bed5d1d706038d6ee7fec4529d069db
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-30 00:37:24 +0400

    feat: Testing Stack class

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,75 @@
+﻿using NuGet.Frameworks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class StackTests
+{
+    [Test]
+    public void Push_ArgIsNull_ThrowArgNullException()
+    {
+        var stack = new Fundamentals.Stack<string>();
+
+        Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Push_ValidArg_AddTheObjectToTheStack()
+    {
+        var stack = new Fundamentals.Stack<string>();
+
+        stack.Push("a");
+
+        Assert.That(stack.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Count_EmptyStack_ReturnZero()
+    {
+        var stack = new Fundamentals.Stack<string>();
+
+        Assert.That(stack.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Pop_EmptyStack_ThrowInvalidOperationException()
+    {
+        var stack = new Fundamentals.Stack<string>();
+
+        Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+    }
+
+    [Test]
+    public void Pop_StackWithAFewObjects_ReturnObjectOnTheTop()
+    {
+        // Arrange
+        var stack = new Fundamentals.Stack<string>();
+        stack.Push("a");
+        stack.Push("b");
+        stack.Push("c");
+        
+        // Act
+        var result = stack.Pop();
+        
+        // Assert 
+        Assert.That(result, Is.EqualTo("c"));
+    }
+
+    [Test]
+    public void Pop_StackWithAFewObjects_RemoveObjectOnTheTop()
+    {
+        // Arrange
+        var stack = new Fundamentals.Stack<string>();
+        stack.Push("a");
+        stack.Push("b");
+        stack.Push("c");
+        
+        // Act
+        stack.Pop();
+        
+        //Assert
+        Assert.That(stack.Count, Is.EqualTo(2));
+    }
+}
