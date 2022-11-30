9b3080888be53f07d030d41717639b98e18c31ea
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-30 22:50:34 +0400

    feat: Refactoring Towards a Loosely-coupled Design

---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,14 @@
+﻿namespace TestNinja.Mocking;
+
+public interface IFileReader
+{
+    string Read(string path);
+}
+
+public class FileReader : IFileReader
+{
+    public string Read(string path)
+    {
+        return File.ReadAllText(path);
+    }
+}
---
+++ TestNinja/Mocking/VideoService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace TestNinja.Mocking;
+
+public class VideoService
+{
+    public string ReadVideoTitle()
+    {
+        var str = new FileReader().Read("video.txt");
+        var video = JsonConvert.DeserializeObject<Video>(str);
+        if (video == null)
+            return "Error parsing the video.";
+
+        return video.Title;
+    }
+}
+
+public class Video
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsProcessed { get; set; }
+}
+
+public class VideoContext : DbContext
+{
+    public DbSet<Video> Videos { get; set; }
+}
